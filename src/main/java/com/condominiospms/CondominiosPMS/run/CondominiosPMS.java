a50d4cfbe947225c1045b5dcc4067297053d532c
Joao12 <132232545+JoaoConde12@users.noreply.github.com.>
2026-06-01 20:41:25 -0500

    feat: primer commit

---
+++ src/main/java/com/condominiospms/CondominiosPMS/run/CondominiosPMS.java
@@ -0,0 +1,16 @@
+package com.condominiospms.CondominiosPMS.run;
+
+import org.openxava.util.*;
+
+/**
+ * Ejecuta esta clase para arrancar la aplicación.
+ */
+
+public class CondominiosPMS {
+
+	public static void main(String[] args) throws Exception {
+		DBServer.start("CondominiosPMS-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		AppServer.run("CondominiosPMS"); // Usa AppServer.run("") para funcionar en el contexto raíz
+	}
+
+}
